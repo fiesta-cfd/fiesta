29c343caa2d9b46bf73e5654cd07b35f959d7c52
Brian Romero <beromer@unm.edu>
2019-09-05 12:37:02 -0600

    adding environment initialization script that sets up the environment variables and loads the required modules

--- src/input.lua
+++ src/input.lua
@@ -7,13 +7,10 @@ nk = 10
 dx = L/ni
 dy = L/nj
 dz = L/nk
-procsx = 1
-procsy = 1
-procsz = 1
+procsx = 2
+procsy = 2
+procsz = 2
 
 function f(x,y)
-    --return(x^2 * math.sin(y))/(1-x)
-    local rtn = (math.pi*x + math.pi*y)
-    return rtn
-    --return (x/y)
+    return(x^2 * math.sin(y))/(1-x)
 end
