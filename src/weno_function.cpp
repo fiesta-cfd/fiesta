76c76d50948206291514760eec3ee290ba7d886e
Brian Romero <beromer@unm.edu>
2019-09-27 19:41:38 -0600

    weno rewrite is complete and code is now 30% faster

--- src/weno_function.cpp
+++ src/weno_function.cpp
@@ -41,7 +41,7 @@ struct calculateRhoAndGamma {
         double Cp = 0;
         double Cv = 0;
 
-        rho(i,j,k) = 0;
+        rho(i,j,k) = 0.0;
 
         for (int s=0; s<ns; ++s){
             rho(i,j,k) = rho(i,j,k) + var(i,j,k,4+s);
@@ -63,42 +63,79 @@ struct calculateVelocityAndPressure {
     typedef typename Kokkos::View<double****> V4D;
     typedef typename Kokkos::View<double***> V3D;
     V4D var;
-    V3D uvel;
-    V3D vvel;
-    V3D wvel;
+    //V3D uvel;
+    //V3D vvel;
+    //V3D wvel;
     V3D p;
     V3D rho;
-    V3D gamma;
+    //V3D gamma;
+    Kokkos::View<double*> cd;
 
-    calculateVelocityAndPressure (V4D var_,  V3D uvel_, V3D vvel_, V3D wvel_, V3D p_, V3D rho_, V3D gamma_)
-         : var(var_), uvel(uvel_), vvel(vvel_), wvel(wvel_), p(p_), rho(rho_), gamma(gamma_) {}
+    //calculateVelocityAndPressure (V4D var_,  V3D uvel_, V3D vvel_, V3D wvel_, V3D p_, V3D rho_, V3D gamma_)
+    calculateVelocityAndPressure (V4D var_, V3D p_, V3D rho_, Kokkos::View<double*> cd_)
+         : var(var_), p(p_), rho(rho_), cd(cd_) {}
 
     KOKKOS_INLINE_FUNCTION
     void operator()(const int i, const int j, const int k) const {
-        uvel(i,j,k) = var(i,j,k,0)/rho(i,j,k);
-        vvel(i,j,k) = var(i,j,k,1)/rho(i,j,k);
-        wvel(i,j,k) = var(i,j,k,2)/rho(i,j,k);
+        //uvel(i,j,k) = var(i,j,k,0)/rho(i,j,k);
+        //vvel(i,j,k) = var(i,j,k,1)/rho(i,j,k);
+        //wvel(i,j,k) = var(i,j,k,2)/rho(i,j,k);
 
-        p(i,j,k) = (gamma(i,j,k)-1)*(var(i,j,k,3)-0.5*rho(i,j,k)
-                  *(uvel(i,j,k)*uvel(i,j,k) + vvel(i,j,k)*vvel(i,j,k) + wvel(i,j,k)*wvel(i,j,k)));
+        //p(i,j,k) = (gamma(i,j,k)-1)*( var(i,j,k,3) - 0.5*rho(i,j,k)
+        //          *(uvel(i,j,k)*uvel(i,j,k) + vvel(i,j,k)*vvel(i,j,k) + wvel(i,j,k)*wvel(i,j,k)) );
+
+        int ns = (int)cd(0);
+        double gamma, gammas, Rs;
+        double Cp = 0;
+        double Cv = 0;
+
+        rho(i,j,k) = 0.0;
+
+        for (int s=0; s<ns; ++s){
+            rho(i,j,k) = rho(i,j,k) + var(i,j,k,4+s);
+        }
+
+        for (int s=0; s<ns; ++s){
+            gammas = cd(4+2*s);
+            Rs = cd(4+2*s+1);
+
+            Cp = Cp + (var(i,j,k,4+s)/rho(i,j,k))*( gammas*Rs/(gammas-1) );
+            Cv = Cv + (var(i,j,k,4+s)/rho(i,j,k))*( Rs/(gammas-1) );
+        }
+
+        gamma = Cp/Cv;
+        //gamma = 1.4;
+
+        p(i,j,k) = (gamma-1)*( var(i,j,k,3) - (0.5/rho(i,j,k))
+                  *(var(i,j,k,0)*var(i,j,k,0) + var(i,j,k,1)*var(i,j,k,1) + var(i,j,k,2)*var(i,j,k,2)) );
     }
 };
 
-struct applyWenoFluxes {
+struct calculateWenoFluxes {
     
     typedef typename Kokkos::View<double****> V4D;
     typedef typename Kokkos::View<double***> V3D;
     V4D var;
-    V3D uvel;
-    V3D vvel;
-    V3D wvel;
+    //V3D uvel;
+    //V3D vvel;
+    //V3D wvel;
     V3D p;
-    V4D dvar;
+    V3D rho;
+    V3D wenox;
+    V3D wenoy;
+    V3D wenoz;
     Kokkos::View<double*> cd;
+    int v;
     double eps = 0.000001;
 
-    applyWenoFluxes (V4D var_, V3D uvel_, V3D vvel_, V3D wvel_, V3D p_, V4D dvar_, Kokkos::View<double*> cd_)
-         : var(var_), uvel(uvel_), vvel(vvel_), wvel(wvel_), p(p_), dvar(dvar_), cd(cd_) {}
+    //calculateWenoFluxes (V4D var_, V3D uvel_, V3D vvel_, V3D wvel_, V3D p_,
+    //                     V3D wenox_, V3D wenoy_, V3D wenoz_, Kokkos::View<double*> cd_, int v_)
+    //                     : var(var_), uvel(uvel_), vvel(vvel_), wvel(wvel_), p(p_),
+    //                       wenox(wenox_), wenoy(wenoy_), wenoz(wenoz_), cd(cd_), v(v_) {}
+    calculateWenoFluxes (V4D var_, V3D p_, V3D rho_,
+                         V3D wenox_, V3D wenoy_, V3D wenoz_, Kokkos::View<double*> cd_, int v_)
+                         : var(var_), p(p_), rho(rho_),
+                           wenox(wenox_), wenoy(wenoy_), wenoz(wenoz_), cd(cd_), v(v_) {}
     
     KOKKOS_INLINE_FUNCTION
     void operator()(const int i, const int j, const int k) const {
@@ -106,83 +143,112 @@ struct applyWenoFluxes {
         int ns = (int)cd(0);
         double ur,vr,wr,w,b1,b2,b3,w1,w2,w3,p1,p2,p3,f1,f2,f3,f4,f5;
 
-        ur = (-uvel(i+2,j,k) + 7.0*uvel(i+1,j,k) + 7.0*uvel(i,j,k) - uvel(i-1,j,k))/12.0;
-        vr = (-vvel(i,j+2,k) + 7.0*vvel(i,j+1,k) + 7.0*vvel(i,j,k) - vvel(i,j-1,k))/12.0;
-        wr = (-wvel(i,j,k+2) + 7.0*wvel(i,j,k+1) + 7.0*wvel(i,j,k) - wvel(i,j,k-1))/12.0;
+        //ur = (-uvel(i+2,j,k) + 7.0*uvel(i+1,j,k) + 7.0*uvel(i,j,k) - uvel(i-1,j,k))/12.0;
+        //vr = (-vvel(i,j+2,k) + 7.0*vvel(i,j+1,k) + 7.0*vvel(i,j,k) - vvel(i,j-1,k))/12.0;
+        //wr = (-wvel(i,j,k+2) + 7.0*wvel(i,j,k+1) + 7.0*wvel(i,j,k) - wvel(i,j,k-1))/12.0;
+        ur = ( -     var(i+2,j,k,0)/rho(i+2,j,k) + 7.0*var(i+1,j,k,0)/rho(i+1,j,k)
+               + 7.0*var(i  ,j,k,0)/rho(i  ,j,k) -     var(i-1,j,k,0)/rho(i-1,j,k) )/12.0;
+
+        vr = ( -     var(i,j+2,k,1)/rho(i,j+2,k) + 7.0*var(i,j+1,k,1)/rho(i,j+1,k)
+               + 7.0*var(i,j  ,k,1)/rho(i,j  ,k) -     var(i,j-1,k,1)/rho(i,j-1,k) )/12.0;
+
+        wr = ( -     var(i,j,k+2,2)/rho(i,j,k+2) + 7.0*var(i,j,k+1,2)/rho(i,j,k+1)
+               + 7.0*var(i,j,k  ,2)/rho(i,j,k  ) -     var(i,j,k-1,2)/rho(i,j,k-1) )/12.0;
         
-        for (int v=0; v<=4+ns; ++v){
-            for (int idx=0; idx<3; ++idx){
-                if (idx == 0){
-                    if (ur < 0.0){
-                        f1 = var(i+3,j,k,v); + (v==3)*p(i+3,j,k);
-                        f2 = var(i+2,j,k,v); + (v==3)*p(i+2,j,k);
-                        f3 = var(i+1,j,k,v); + (v==3)*p(i+1,j,k);
-                        f4 = var(i  ,j,k,v); + (v==3)*p(i  ,j,k);
-                        f5 = var(i-1,j,k,v); + (v==3)*p(i-1,j,k);
-                    }else{                 ;               
-                        f1 = var(i-2,j,k,v); + (v==3)*p(i-2,j,k);
-                        f2 = var(i-1,j,k,v); + (v==3)*p(i-1,j,k);
-                        f3 = var(i  ,j,k,v); + (v==3)*p(i  ,j,k);
-                        f4 = var(i+1,j,k,v); + (v==3)*p(i+1,j,k);
-                        f5 = var(i+2,j,k,v); + (v==3)*p(i+2,j,k);
-                    }
-                } else if (idx == 1) {
-                    if (vr < 0.0){
-                        f1 = var(i,j+3,k,v); + (v==3)*p(i,j+3,k);
-                        f2 = var(i,j+2,k,v); + (v==3)*p(i,j+2,k);
-                        f3 = var(i,j+1,k,v); + (v==3)*p(i,j+1,k);
-                        f4 = var(i,j  ,k,v); + (v==3)*p(i,j  ,k);
-                        f5 = var(i,j-1,k,v); + (v==3)*p(i,j-1,k);
-                    }else{                 ;                 
-                        f1 = var(i,j-2,k,v); + (v==3)*p(i,j-2,k);
-                        f2 = var(i,j-1,k,v); + (v==3)*p(i,j-1,k);
-                        f3 = var(i,j  ,k,v); + (v==3)*p(i,j  ,k);
-                        f4 = var(i,j+1,k,v); + (v==3)*p(i,j+1,k);
-                        f5 = var(i,j+2,k,v); + (v==3)*p(i,j+2,k);
-                    }
-                } else {
-                    if (wr < 0.0){
-                        f1 = var(i,j,k+3,v); + (v==3)*p(i,j,k+3);
-                        f2 = var(i,j,k+2,v); + (v==3)*p(i,j,k+2);
-                        f3 = var(i,j,k+1,v); + (v==3)*p(i,j,k+1);
-                        f4 = var(i,j,k  ,v); + (v==3)*p(i,j,k  );
-                        f5 = var(i,j,k-1,v); + (v==3)*p(i,j,k-1);
-                    }else{                 ;                   
-                        f1 = var(i,j,k-2,v); + (v==3)*p(i,j,k-2);
-                        f2 = var(i,j,k-1,v); + (v==3)*p(i,j,k-1);
-                        f3 = var(i,j,k  ,v); + (v==3)*p(i,j,k  );
-                        f4 = var(i,j,k+1,v); + (v==3)*p(i,j,k+1);
-                        f5 = var(i,j,k+2,v); + (v==3)*p(i,j,k+2);
-                    }
+        for (int idx=0; idx<3; ++idx){
+            if (idx == 0){
+                if (ur < 0.0){
+                    f1 = var(i+3,j,k,v) + (v==3)*p(i+3,j,k);
+                    f2 = var(i+2,j,k,v) + (v==3)*p(i+2,j,k);
+                    f3 = var(i+1,j,k,v) + (v==3)*p(i+1,j,k);
+                    f4 = var(i  ,j,k,v) + (v==3)*p(i  ,j,k);
+                    f5 = var(i-1,j,k,v) + (v==3)*p(i-1,j,k);
+                }else{                                
+                    f1 = var(i-2,j,k,v) + (v==3)*p(i-2,j,k);
+                    f2 = var(i-1,j,k,v) + (v==3)*p(i-1,j,k);
+                    f3 = var(i  ,j,k,v) + (v==3)*p(i  ,j,k);
+                    f4 = var(i+1,j,k,v) + (v==3)*p(i+1,j,k);
+                    f5 = var(i+2,j,k,v) + (v==3)*p(i+2,j,k);
                 }
-
-                b1 = (13/12)*pow((f1-2.0*f2+f3),2.0) + (0.25)*pow((f1-4.0*f2+3.0*f3),2.0);
-                b2 = (13/12)*pow((f2-2.0*f3+f4),2.0) + (0.25)*pow((f2-f4),2.0);
-                b3 = (13/12)*pow((f3-2.0*f4+f5),2.0) + (0.25)*pow((3.0*f3-4.0*f4+f5),2.0);
-
-                w1 = (0.1)/pow((eps+b1),2.0);
-                w2 = (0.6)/pow((eps+b2),2.0);
-                w3 = (0.3)/pow((eps+b3),2.0);
-
-                p1 = ( 1.0/3.0)*f1 + (-7.0/6.0)*f2 + (11.0/6.0)*f3;
-                p2 = (-1.0/6.0)*f2 + ( 5.0/6.0)*f3 + ( 1.0/3.0)*f4;
-                p3 = ( 1.0/3.0)*f3 + ( 5.0/6.0)*f4 + (-1.0/6.0)*f5;
-
-                w = (w1*p1+w2*p2+w3*p3)/(w1+w2+w3);
-
-                if (idx == 0){
-                    dvar(i,j,k,v) = dvar(i,j,k,v) - ur*w/cd(1);
-                    dvar(i+1,j,k,v) = dvar(i+1,j,k,v) + ur*w/cd(1);
-                } else if (idx == 1) {
-                    dvar(i,j,k,v) = dvar(i,j,k,v) - vr*w/cd(2);
-                    dvar(i,j+1,k,v) = dvar(i,j+1,k,v) + vr*w/cd(2);
-                } else {
-                    dvar(i,j,k,v) = dvar(i,j,k,v) - wr*w/cd(3);
-                    dvar(i,j,k+1,v) = dvar(i,j,k+1,v) + wr*w/cd(3);
+            } 
+            if (idx == 1) {
+                if (vr < 0.0){
+                    f1 = var(i,j+3,k,v) + (v==3)*p(i,j+3,k);
+                    f2 = var(i,j+2,k,v) + (v==3)*p(i,j+2,k);
+                    f3 = var(i,j+1,k,v) + (v==3)*p(i,j+1,k);
+                    f4 = var(i,j  ,k,v) + (v==3)*p(i,j  ,k);
+                    f5 = var(i,j-1,k,v) + (v==3)*p(i,j-1,k);
+                }else{                                  
+                    f1 = var(i,j-2,k,v) + (v==3)*p(i,j-2,k);
+                    f2 = var(i,j-1,k,v) + (v==3)*p(i,j-1,k);
+                    f3 = var(i,j  ,k,v) + (v==3)*p(i,j  ,k);
+                    f4 = var(i,j+1,k,v) + (v==3)*p(i,j+1,k);
+                    f5 = var(i,j+2,k,v) + (v==3)*p(i,j+2,k);
                 }
-            } //idx
-        }// vv
-    }//operator()
+            }
+            if (idx == 2){
+                if (wr < 0.0){
+                    f1 = var(i,j,k+3,v) + (v==3)*p(i,j,k+3);
+                    f2 = var(i,j,k+2,v) + (v==3)*p(i,j,k+2);
+                    f3 = var(i,j,k+1,v) + (v==3)*p(i,j,k+1);
+                    f4 = var(i,j,k  ,v) + (v==3)*p(i,j,k  );
+                    f5 = var(i,j,k-1,v) + (v==3)*p(i,j,k-1);
+                }else{                                    
+                    f1 = var(i,j,k-2,v) + (v==3)*p(i,j,k-2);
+                    f2 = var(i,j,k-1,v) + (v==3)*p(i,j,k-1);
+                    f3 = var(i,j,k  ,v) + (v==3)*p(i,j,k  );
+                    f4 = var(i,j,k+1,v) + (v==3)*p(i,j,k+1);
+                    f5 = var(i,j,k+2,v) + (v==3)*p(i,j,k+2);
+                }
+            }
+
+            b1 = (13/12)*pow((f1-2.0*f2+f3),2.0) + (0.25)*pow((f1-4.0*f2+3.0*f3),2.0);
+            b2 = (13/12)*pow((f2-2.0*f3+f4),2.0) + (0.25)*pow((f2-f4),2.0);
+            b3 = (13/12)*pow((f3-2.0*f4+f5),2.0) + (0.25)*pow((3.0*f3-4.0*f4+f5),2.0);
+
+            w1 = (0.1)/pow((eps+b1),2.0);
+            w2 = (0.6)/pow((eps+b2),2.0);
+            w3 = (0.3)/pow((eps+b3),2.0);
+
+            p1 = ( 1.0/3.0)*f1 + (-7.0/6.0)*f2 + (11.0/6.0)*f3;
+            p2 = (-1.0/6.0)*f2 + ( 5.0/6.0)*f3 + ( 1.0/3.0)*f4;
+            p3 = ( 1.0/3.0)*f3 + ( 5.0/6.0)*f4 + (-1.0/6.0)*f5;
+
+            w = (w1*p1+w2*p2+w3*p3)/(w1+w2+w3);
+
+            if (idx == 0){
+                wenox(i,j,k) = ur*w/cd(1);
+            }
+            if (idx == 1) {
+                wenoy(i,j,k) = vr*w/cd(2);
+            }
+            if (idx == 2) {
+                wenoz(i,j,k) = wr*w/cd(3);
+            }
+        }
+    }
+};
+
+struct applyWenoFluxes {
+    
+    typedef typename Kokkos::View<double****> V4D;
+    typedef typename Kokkos::View<double***> V3D;
+    V4D dvar;
+    V3D wenox;
+    V3D wenoy;
+    V3D wenoz;
+    int v;
+
+    applyWenoFluxes (V4D dvar_, V3D wenox_, V3D wenoy_, V3D wenoz_, int v_)
+        : dvar(dvar_), wenox(wenox_), wenoy(wenoy_), wenoz(wenoz_), v(v_) {}
+    
+    KOKKOS_INLINE_FUNCTION
+    void operator()(const int i, const int j, const int k) const {
+
+        dvar(i,j,k,v) = -( (wenox(i,j,k) - wenox(i-1,j,k))
+                          +(wenoy(i,j,k) - wenoy(i,j-1,k))
+                          +(wenoz(i,j,k) - wenoz(i,j,k-1)) );
+    }
 };
 
 struct applyPressure {
@@ -221,24 +287,34 @@ void weno_func::operator()() {
     typedef typename Kokkos::View<double****> V4D;
     typedef typename Kokkos::View<double***> V3D;
 
-    V3D uvel("uvel",cf.ngi,cf.ngj,cf.ngk);
-    V3D vvel("vvel",cf.ngi,cf.ngj,cf.ngk);
-    V3D wvel("wvel",cf.ngi,cf.ngj,cf.ngk);
+    //V3D uvel("uvel",cf.ngi,cf.ngj,cf.ngk);
+    //V3D vvel("vvel",cf.ngi,cf.ngj,cf.ngk);
+    //V3D wvel("wvel",cf.ngi,cf.ngj,cf.ngk);
     V3D p("p",cf.ngi,cf.ngj,cf.ngk);
     V3D rho("rho",cf.ngi,cf.ngj,cf.ngk);
-    V3D gamma("gamma",cf.ngi,cf.ngj,cf.ngk);
+    V3D wenox("wenox",cf.ngi,cf.ngj,cf.ngk);
+    V3D wenoy("wenoy",cf.ngi,cf.ngj,cf.ngk);
+    V3D wenoz("wenoz",cf.ngi,cf.ngj,cf.ngk);
+    //V3D gamma("gamma",cf.ngi,cf.ngj,cf.ngk);
 
-    Kokkos::parallel_for(policy_f4({0,0,0,0},{cf.ngi, cf.ngj, cf.ngk, cf.nv}),
-        initializeDvar (dvar));
+    //Kokkos::parallel_for(policy_f4({0,0,0,0},{cf.ngi, cf.ngj, cf.ngk, cf.nv}),
+    //    initializeDvar (dvar));
+
+    //Kokkos::parallel_for(policy_f({0,0,0},{cf.ngi, cf.ngj, cf.ngk}),
+    //    calculateRhoAndGamma (var,rho,gamma,cd));
 
     Kokkos::parallel_for(policy_f({0,0,0},{cf.ngi, cf.ngj, cf.ngk}),
-        calculateRhoAndGamma (var,rho,gamma,cd));
+        calculateVelocityAndPressure (var,p,rho,cd));
+        //calculateVelocityAndPressure (var,uvel,vvel,wvel,p,rho,gamma));
 
-    Kokkos::parallel_for(policy_f({0,0,0},{cf.ngi, cf.ngj, cf.ngk}),
-        calculateVelocityAndPressure (var,uvel,vvel,wvel,p,rho,gamma));
+    for (int v=0; v<cf.nv; ++v){
+        Kokkos::parallel_for(policy_f({cf.ng-1,cf.ng-1,cf.ng-1},{cf.ngi-cf.ng, cf.ngj-cf.ng, cf.ngk-cf.ng}),
+            calculateWenoFluxes (var,p,rho,wenox,wenoy,wenoz,cd,v));
+            //calculateWenoFluxes (var,uvel,vvel,wvel,p,wenox,wenoy,wenoz,cd,v));
 
-    Kokkos::parallel_for(policy_f({cf.ng-1,cf.ng-1,cf.ng-1},{cf.ngi-cf.ng, cf.ngj-cf.ng, cf.ngk-cf.ng}),
-        applyWenoFluxes (var,uvel,vvel,wvel,p,dvar,cd));
+        Kokkos::parallel_for(policy_f({cf.ng,cf.ng,cf.ng},{cf.ngi-cf.ng, cf.ngj-cf.ng, cf.ngk-cf.ng}),
+            applyWenoFluxes (dvar,wenox,wenoy,wenoz,v));
+    }
 
     Kokkos::parallel_for(policy_f({cf.ng,cf.ng,cf.ng},{cf.ngi-cf.ng, cf.ngj-cf.ng, cf.ngk-cf.ng}),
         applyPressure (dvar,p,cd));
