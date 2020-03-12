7952b1304914f17262c27510c7117d4e18f28edd
Brian Romero <beromer@unm.edu>
2020-03-12 15:34:03 -0600

    changed master document

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,6 +24,8 @@ author = 'Brian Romero'
 # The full version, including alpha/beta/rc tags
 release = '1.0.0'
 
+master_doc='index'
+
 
 # -- General configuration ---------------------------------------------------
 
