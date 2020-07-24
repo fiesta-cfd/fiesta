4280a913d04cd2b21e978a3e6ee5b07deb4dc09d
Sang Yoon Byun <byunsang@ko-fe1.localdomain>
2020-07-24 10:49:18 -0600

    Added comments to cart3d.hpp and gen3d.hpp

--- src/cart3d.hpp
+++ src/cart3d.hpp
@@ -14,20 +14,20 @@ public:
     void postSim();
 
     FS3D p;		// Pressure
-    FS3D T;
-    FS3D rho;
-    FS3D qx;
-    FS3D qy;
-    FS3D qz;
-    FS3D fluxx;
-    FS3D fluxy;
-    FS3D fluxz;
-    FS5D stressx;
-    FS5D stressy;
-    FS5D stressz;
-    FS4D gradRho;
+    FS3D T;             // Temperature
+    FS3D rho;           // Total Density
+    FS3D qx;            // Heat Fluxes in X direction
+    FS3D qy;            // Heat Fluxes in Y direction  
+    FS3D qz;            // Heat Fluxes in Z direction
+    FS3D fluxx;         // Weno Fluxes in X direction
+    FS3D fluxy;         // Weno Fluxes in Y direction
+    FS3D fluxz;         // Weno Fluxes in Z direction
+    FS5D stressx;       // Stress tensor on X faces
+    FS5D stressy;       // Stress tensor on Y faces
+    FS5D stressz;       // Stress tensor on Z faces
+    FS4D gradRho;       // Density Gradient array
     FS4D cFlux;
     FS6D mFlux;
-    FS1D cd;
+    FS1D cd;            // Device configuration array
 
 };
