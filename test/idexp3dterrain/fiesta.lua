60315719ad3df225df77d71f28aa6ff9627dbd79
Brian Romero <beromer@unm.edu>
2020-09-27 16:16:43 -0600

    added 3d terrain problem

---
+++ test/idexp3dterrain/fiesta.lua
@@ -0,0 +1,110 @@
+--
+-- 3D Ideal Expansion
+--
+
+title = "3D Idealized Expansion"
+
+--Restart and Output Options
+restart_freq = 0                      --Restart Write Interval
+write_freq = 10                      --Solution Write Interval
+stat_freq = 10
+restart = 0                           --Whether or not to use restart file
+time = 0.0                            --Start time of simulation
+tstart = 0                            --Start time index of simulation
+restartName = "restart-000000.cgns"   --Restart File Name
+
+--Gas Properties
+R = 8.314462                          --Universal Gas Constant [J/(K*mol)]
+ns = 1                                --Number of Gas Species
+species_names={"Air"}
+gamma = {1.400}                  --Array of Species Ratio of Specific Heats
+M = {0.02897}                 --Array of Species Molar Masses [kg/mol]
+mu = {2.928e-5}
+visc=0
+scheme="weno5"
+grid="terrain"
+buoyancy=1
+
+--Time
+nt = 1000                             --Time Step at which to end simulation
+dt = 1e-7                             --Time Step Size [s]
+
+--User Parameters
+Lx = 10.0
+Ly = 10.0
+Lz = 10.0
+
+--Number of cells
+ndim = 3
+ni = 256            --Simulation size in x direction
+nj = 256            --Simulation Size in y direction
+nk = 100            --Simulation Size in z direction
+h = 10
+
+--Cell Sizes
+dx = Lx/ni
+dy = Ly/nj
+dz = Lz/nk
+
+--MPI Processors
+procsx = 1
+procsy = 1
+procsz = 1
+
+--Boundary Conditions (0 - Freeflow, 1 - Reflective)
+bcXmin = 1
+bcXmax = 0
+bcYmin = 1
+bcYmax = 0
+bcZmin = 1
+bcZmax = 0
+
+xPer = 0
+yPer = 0
+zPer = 0
+
+ceq = 0             --Enable/Disable Cequation
+noise = 0
+particle = 0
+
+function g(i,j,k,v)
+    if v==0 then return 2*dx*i end
+    if v==1 then return dy*j end
+    if v==2 then return dz*k end
+end
+
+function f(xi,yi,zi,v)
+    local xdist = math.abs(5-xi)
+    local ydist = math.abs(5-yi)
+    local zdist = math.abs(6-zi)
+    --find radial distance from center
+    local rdist = math.sqrt(xdist*xdist + ydist*ydist + zdist*zdist)
+
+    --calcualte heat capacity of ambient and hot regions
+    local Cv_air = R/(M[1]*(gamma[1]-1))
+    local Cv_hot = R/(M[1]*(gamma[1]-1))
+    
+    --calculate internal energies
+    --Air at 300[k] 1.0[kg/m^3]
+    Eair = 300 *Cv_air*1.0
+    --Air at 3000[k] 1000[kg/m^3]
+    Ehot = 3000*Cv_hot*1000.0
+
+    --setup hot region
+    if rdist < 0.5 then
+        if v==0 then return 0      end
+        if v==1 then return 0      end
+        if v==2 then return 0      end
+        if v==3 then return Ehot   end
+        if v==4 then return 1000.0 end
+        if v==5 then return 0      end
+    --ambient conditions
+    else
+        if v==0 then return 0      end
+        if v==1 then return 0      end
+        if v==2 then return 0      end
+        if v==3 then return Eair   end
+        if v==4 then return 1.0000 end
+        if v==5 then return 0      end
+    end
+end
---
+++ test/idexp3dterrain/genTerrain.py
@@ -0,0 +1,102 @@
+#! python3
+
+import numpy as np
+import h5py
+import math
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D
+from random import random
+from random import seed
+
+np.set_printoptions(linewidth=1000)
+levels = 8
+amp = 1.0
+sd = 468
+
+sz = 2**levels+1
+z = np.zeros((sz,sz))
+seed(sd)
+
+z[0,0] = 1.5
+z[0,-1] = 2.5
+z[-1,0] = 1.5
+z[-1,-1] = 5.0
+
+print('Generating Terrain...')
+for s in range(levels):
+    amp = amp/((s+1)**(1.0/2.0))
+    skip = 2**(levels-s)
+    for j in range(2**s):
+        for i in range(2**s):
+            skh = int(skip/2)
+            li = i*skip
+            lj = j*skip
+            ci = int(li+skip/2)
+            cj = int(lj+skip/2)
+            c = z[li,lj]
+            c += z[li+skip,lj]
+            c += z[li,lj+skip]
+            c += z[li+skip,lj+skip]
+            z[ci,cj] = (c/4.0) + 2*amp*random()-amp
+
+            di = ci-skh
+            dj = cj
+            c  = z[di+skh,dj]
+            c += z[di,dj-skh]
+            c += z[di,dj+skh]
+            if di-skh >= 0:
+                c += z[di-skh,dj]
+                z[di,dj] = (c/4.0) + 2*amp*random()-amp
+            else:
+                z[di,dj] = (c/3.0) + 2*amp*random()-amp
+
+            di = ci+skh
+            dj = cj
+            c = z[di-skh,dj]
+            c += z[di,dj-skh]
+            c += z[di,dj+skh]
+            if di+skh < sz:
+                c += z[di+skh,dj]
+                z[di,dj] = (c/4.0) + 2*amp*random()-amp
+            else:
+                z[di,dj] = (c/3.0) + 2*amp*random()-amp
+
+            di = ci
+            dj = cj-skh
+            c  = z[di+skh,dj]
+            c += z[di,dj+skh]
+            c += z[di-skh,dj]
+            if dj-skh >= 0:
+                c += z[di,dj-skh]
+                z[di,dj] = (c/4.0) + 2*amp*random()-amp
+            else:
+                z[di,dj] = (c/3.0) + 2*amp*random()-amp
+
+            di = ci
+            dj = cj+skh
+            c = z[di-skh,dj]
+            c += z[di,dj-skh]
+            c += z[di+skh,dj]
+            if dj+skh < sz:
+                c += z[di,dj+skh]
+                z[di,dj] = (c/4.0) + 2*amp*random()-amp
+            else:
+                z[di,dj] = (c/3.0) + 2*amp*random()-amp
+
+print("Writing data...")
+hf = h5py.File('terrain.h5','w')
+hf.create_dataset('Height',data=z)
+hf.close()
+
+print("Plotting...")
+x=np.linspace(0,10,sz)
+y=np.linspace(0,10,sz)
+x,y=np.meshgrid(x,y)
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+
+surf = ax.plot_surface(x,y,z,cmap=cm.terrain,edgecolor='none',rstride=2,cstride=2)
+ax.set_zlim(0,10)
+plt.show()
