f55bb7f26ea33c6733794300f777d89cabe35227
Brian Romero <bromero@unm.edu>
2021-07-29 01:38:44 -0600

    feat(tets): add periodic bc test

--- tests/test.cpp
+++ tests/test.cpp
@@ -14,16 +14,16 @@ rk_func * initTests(struct inputConfig& cf){
   cf.glbl_ncj=3;
   cf.glbl_nck=3;
   cf.ng=3;
-  cf.xPer=0;
-  cf.yPer=0;
-  cf.zPer=0;
+  //cf.xPer=0;
+  //cf.yPer=0;
+  //cf.zPer=0;
   cf.nvt=4;
   cf.nv=4;
 
-  cf.ceq=0;
-  cf.noise=0;
-  cf.visc=0;
-  cf.gravity=0;
+  cf.ceq=false;
+  cf.noise=false;
+  cf.visc=false;
+  cf.buoyancy=false;
 
   cf.xProcs=1;
   cf.yProcs=1;
@@ -60,12 +60,12 @@ TEST_CASE( "Outflow BC Test", "[outflow_bc]" ) {
   struct inputConfig cf;
   rk_func *f = initTests(cf);
 
-  cf.bcR=0;
-  cf.bcL=0;
-  cf.bcB=0;
-  cf.bcT=0;
-  cf.bcH=0;
-  cf.bcF=0;
+  cf.bcR=BCType::outflow;
+  cf.bcL=BCType::outflow;
+  cf.bcB=BCType::outflow;
+  cf.bcT=BCType::outflow;
+  cf.bcH=BCType::outflow;
+  cf.bcF=BCType::outflow;
 
   FS4DH varH = Kokkos::create_mirror_view(f->var);
 
@@ -121,12 +121,12 @@ TEST_CASE( "Outflow BC Test", "[outflow_bc]" ) {
 TEST_CASE( "Refelctive BC Test", "[reflective_bc]" ) {
   struct inputConfig cf;
   rk_func *f = initTests(cf);
-  cf.bcR=1;
-  cf.bcL=1;
-  cf.bcB=1;
-  cf.bcT=1;
-  cf.bcH=1;
-  cf.bcF=1;
+  cf.bcR=BCType::reflective;
+  cf.bcL=BCType::reflective;
+  cf.bcB=BCType::reflective;
+  cf.bcT=BCType::reflective;
+  cf.bcH=BCType::reflective;
+  cf.bcF=BCType::reflective;
 
   FS4DH varH = Kokkos::create_mirror_view(f->var);
 
@@ -205,49 +205,124 @@ TEST_CASE( "Hydrostatic BC Test", "[hydrostatic_bc]" ) {
   varH(4,5,4,1) = 0;
   varH(5,5,4,1) = 0;
 
-  varH(3,3,4,3) = 1;
-  varH(4,3,4,3) = 1;
-  varH(5,3,4,3) = 1;
-  varH(3,4,4,3) = 1;
-  varH(4,4,4,3) = 1;
-  varH(5,4,4,3) = 1;
-  varH(3,5,4,3) = 1;
-  varH(4,5,4,3) = 1;
-  varH(5,5,4,3) = 1;
+  varH(3,3,4,2) = 0;
+  varH(4,3,4,2) = 0;
+  varH(5,3,4,2) = 0;
+  varH(3,4,4,2) = 0;
+  varH(4,4,4,2) = 0;
+  varH(5,4,4,2) = 0;
+  varH(3,5,4,2) = 0;
+  varH(4,5,4,2) = 0;
+  varH(5,5,4,2) = 0;
 
-  varH(3,3,4,2) = 1;
-  varH(4,3,4,2) = 2;
-  varH(5,3,4,2) = 3;
-  varH(3,4,4,2) = 4;
-  varH(4,4,4,2) = 5;
-  varH(5,4,4,2) = 6;
-  varH(3,5,4,2) = 7;
-  varH(4,5,4,2) = 8;
-  varH(5,5,4,2) = 9;
+  varH(3,3,4,3) = 1;
+  varH(4,3,4,3) = 2;
+  varH(5,3,4,3) = 3;
+  varH(3,4,4,3) = 4;
+  varH(4,4,4,3) = 5;
+  varH(5,4,4,3) = 6;
+  varH(3,5,4,3) = 7;
+  varH(4,5,4,3) = 8;
+  varH(5,5,4,3) = 9;
+
+  varH(3,3,4,4) = 1;
+  varH(4,3,4,4) = 1;
+  varH(5,3,4,4) = 1;
+  varH(3,4,4,4) = 1;
+  varH(4,4,4,4) = 1;
+  varH(5,4,4,4) = 1;
+  varH(3,5,4,4) = 1;
+  varH(4,5,4,4) = 1;
+  varH(5,5,4,4) = 1;
+
   Kokkos::deep_copy(f->var,varH);
 
-  cf.bcR=0;
-  cf.bcL=0;
-  cf.bcB=3;
-  cf.bcT=3;
-  cf.bcH=0;
-  cf.bcF=0;
+  cf.bcR=BCType::outflow;
+  cf.bcL=BCType::outflow;
+  cf.bcB=BCType::hydrostatic;
+  cf.bcT=BCType::hydrostatic;
+  cf.bcH=BCType::outflow;
+  cf.bcF=BCType::outflow;
   //cf.ndim=2;
   applyBCs(cf,f);
 
   Kokkos::deep_copy(varH,f->var);
 
   //e
-  REQUIRE(varH(4,2,4,2)==-1);
-  REQUIRE(varH(4,0,4,2)==-7);
-  REQUIRE(varH(0,4,4,2)==6);
-  REQUIRE(varH(4,8,4,2)==17);
-  REQUIRE(varH(8,4,4,2)==4);
+  REQUIRE(varH(4,2,4,3)==-1);
+  REQUIRE(varH(4,0,4,3)==-7);
+  REQUIRE(varH(0,4,4,3)==6);
+  REQUIRE(varH(4,8,4,3)==17);
+  REQUIRE(varH(8,4,4,3)==4);
 
-  REQUIRE(varH(0,0,4,2)==-6);
-  REQUIRE(varH(8,0,4,2)==-8);
-  REQUIRE(varH(0,8,4,2)==18);
-  REQUIRE(varH(8,8,4,2)==16);
+  REQUIRE(varH(0,0,4,3)==-6);
+  REQUIRE(varH(8,0,4,3)==-8);
+  REQUIRE(varH(0,8,4,3)==18);
+  REQUIRE(varH(8,8,4,3)==16);
+  }
+  Kokkos::finalize();
+  MPI_Finalize();
+}
+
+TEST_CASE( "Periodic BC Test", "[periodic_bc]" ) {
+  {
+  struct inputConfig cf;
+  cf.xPer=1;
+  cf.yPer=1;
+  cf.zPer=1;
+  rk_func *f = initTests(cf);
+
+  FS4DH varH = Kokkos::create_mirror_view(f->var);
+  varH(3,3,4,0) = 1;
+  varH(4,3,4,0) = 2;
+  varH(5,3,4,0) = 3;
+  varH(3,4,4,0) = 4;
+  varH(4,4,4,0) = 5;
+  varH(5,4,4,0) = 6;
+  varH(3,5,4,0) = 7;
+  varH(4,5,4,0) = 8;
+  varH(5,5,4,0) = 9;
+
+  varH(4,3,3,1) = 1;
+  varH(4,3,4,1) = 2;
+  varH(4,3,5,1) = 3;
+  varH(4,4,3,1) = 4;
+  varH(4,4,4,1) = 5;
+  varH(4,4,5,1) = 6;
+  varH(4,5,3,1) = 7;
+  varH(4,5,4,1) = 8;
+  varH(4,5,5,1) = 9;
+
+  Kokkos::deep_copy(f->var,varH);
+
+  cf.bcR=BCType::reflective;
+  cf.bcL=BCType::reflective;
+  cf.bcB=BCType::outflow;
+  cf.bcT=BCType::outflow;
+  cf.bcH=BCType::outflow;
+  cf.bcF=BCType::outflow;
+  applyBCs(cf,f);
+
+  std::cout << cf.xPlus << " : " << cf.xMinus << "\n";
+
+  Kokkos::deep_copy(varH,f->var);
+
+  //e
+  REQUIRE(varH(0,4,4,0)==4);
+  REQUIRE(varH(1,4,4,0)==5);
+  REQUIRE(varH(2,4,4,0)==6);
+  
+  REQUIRE(varH(6,4,4,0)==4);
+  REQUIRE(varH(7,4,4,0)==5);
+  REQUIRE(varH(8,4,4,0)==6);
+
+  REQUIRE(varH(4,4,0,1)==4);
+  REQUIRE(varH(4,4,1,1)==5);
+  REQUIRE(varH(4,4,2,1)==6);
+  
+  REQUIRE(varH(4,4,6,1)==4);
+  REQUIRE(varH(4,4,7,1)==5);
+  REQUIRE(varH(4,4,8,1)==6);
   }
   Kokkos::finalize();
   MPI_Finalize();
