46531dfd08e5fdbbfa6ac37bf85d9768bacb140e
Brian Romero <bromero@unm.edu>
2021-08-18 10:54:50 -0600

    fix(samples): Replace incorrect input file for rt2d

--- samples/rt2d/fiesta.lua
+++ samples/rt2d/fiesta.lua
@@ -1,88 +1,92 @@
-fiesta.title = "2D Idealized Double Bubble"
+fiesta.title = "Two Dimensional Rayleigh-Taylor"
 
---Restart and Output Options
-fiesta.status_frequency = 4000
---fiesta.progress_frequency = 1
-
---restart = 1                           --Whether or not to use restart file
---time = 0.0                            --Start time of simulation
---tstart = 0                            --Start time index of simulation
---restart_name = "./restarts/restart-20.h5"   --Restart File Name
+--Output Options
+fiesta.status_frequency = 5000
 
 --Gas Properties
-fiesta.R = 8.314462                          --Universal Gas Constant [J/(K*mol)]
-fiesta.viscosity.enabled=false
-fiesta.advection_scheme="weno5"
-fiesta.grid.type="cartesian"
-fiesta.buoyancy.enabled=true
+fiesta.R = 1.0                          --Universal Gas Constant [J/(K*mol)]
 
+--Solvers and Physics
+fiesta.advection_scheme="weno5"
+fiesta.viscosity.enabled=false
+fiesta.buoyancy.enabled=true
+fiesta.buoyancy.acceleration=0.1
+
+--Gas Species Data
 fiesta.species={
-    {name="Air",gamma=1.4,M=0.02897,mu=2.928e-5}
+    {name="Air",gamma=1.4,M=1.0,mu=2.928e-5}
 }
 
+--Solution IO Views
 fiesta.ioviews =  {
-    {name="sol",path="./euler_nc_c_test",frequency=4000}
+    {name="sol",path="./solution",frequency=5000}
 }
 
 --Time
-local time = 0.2
-fiesta.time.dt = 0.5e-5
-fiesta.time.nt = 200000
+fiesta.time.dt = 1e-4
+fiesta.time.nt = 7e4
 
+--Grid
+fiesta.grid.type="cartesian"
 fiesta.grid.ndim = 2
-Lx,Ly = 2.0, 2.0
-fiesta.grid.ni       = {128, 128}
+Lx,Ly = 1.0/3.0, 1.0
+fiesta.grid.ni       = {100, 200}
 fiesta.grid.dx       = {Lx/fiesta.grid.ni[1], Ly/fiesta.grid.ni[2]}
-fiesta.mpi.procs    = {1, 1}
+
+--mpi
+fiesta.mpi.procs    = {1,1}
 
 --Boundary Conditions (0 - Freeflow, 1 - Reflective)
-fiesta.bc.xmin = "reflective"
-fiesta.bc.xmax = "reflective"
+fiesta.bc.xperiodic=true
 fiesta.bc.ymin = "hydrostatic"
 fiesta.bc.ymax = "hydrostatic"
 
+--Cequations
 fiesta.ceq.enabled=true
 fiesta.ceq.kappa=10
-fiesta.ceq.epsilon=0.4
-fiesta.ceq.alpha=20
+fiesta.ceq.epsilon=5
+fiesta.ceq.alpha=40
 fiesta.ceq.beta=0
 fiesta.ceq.betae=0
 
+--Noise Filter
 fiesta.noise.enabled=true
 fiesta.noise.dh=2e-5
 fiesta.noise.eta=5e-4
 fiesta.noise.coff=0.05
 fiesta.noise.nt=1
+fiesta.noise.mode=1
 
 function fiesta.initial_conditions(x,y,z,v)
+    h0=height(x-1.0/6.0)
 
-    --Calcualte heat capacity of ambient air and hot air
-    local Cv = fiesta.R/(fiesta.species[1].M*(fiesta.species[1].gamma-1))
+    gamma = fiesta.species[1].gamma
+    g = fiesta.buoyancy.acceleration
 
-    --Calcualte Internal energies
-    --Air at 300[k] 1.0[kg/m^3]
-    Eair = 300*Cv*1.0
+    rU = 2.0 --upper density
+    rL = 1.0 --lower density
 
-    --Air at 3000[k] 1000[kg/m^3]
-    Ehot = 600*Cv*0.5
+    P0=2.4 --"background" pressure
+    pU = P0 + rU*g*(1-y)  --upper pressure
+    pL = P0 + rL*g*(h0-y) + rU*g*(1-h0)  --lower pressure
 
-    if y < zheight(x-1)+1 then
-        --setup hot region
-        if v==0 then return 0      end
-        if v==1 then return 0      end
-        if v==2 then return Ehot   end
-        if v==3 then return 0.5    end
+    eU = pU/(gamma-1)  --upper energy
+    eL = pL/(gamma-1)  --lower energy
+
+    if y > h0 then
+        if v==0 then return 0  end
+        if v==1 then return 0  end
+        if v==2 then return eU end
+        if v==3 then return rU end
     else
-        --ambient conditions
-        if v==0 then return 0      end
-        if v==1 then return 0      end
-        if v==2 then return Eair   end
-        if v==3 then return 1.0    end
+        if v==0 then return 0  end
+        if v==1 then return 0  end
+        if v==2 then return eL end
+        if v==3 then return rL end
     end
 end
 
-function zheight(x)
-    local A = 0.15*math.exp(-16*((x-0.25)^2))
-    local B = 0.15*math.exp(-16*((x+0.25)^2))
-    return A+B
+function height(x)
+    local A = 0.5+0.01*math.cos(6.0*math.pi*x)
+    return A
 end
