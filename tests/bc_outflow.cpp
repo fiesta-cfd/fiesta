14a8002516ddaeaa3166a66f19923935a89722d7
Brian Romero <bromero@unm.edu>
2021-09-15 23:07:07 -0600

    fix(test): ditch catch2

---
+++ tests/bc_outflow.cpp
@@ -0,0 +1,81 @@
+#include "fiesta.hpp"
+#include "input.hpp"
+#include <vector>
+#ifdef HAVE_MPI
+#include "mpi.hpp"
+#endif
+#include "rkfunction.hpp"
+#include "cart3d.hpp"
+#include "bc.hpp"
+#include <iostream>
+#include <cassert>
+#include "debug.hpp"
+#include "test.hpp"
+
+int main(){
+  std::cout << "BEGIN TEST INIT\n";
+  struct inputConfig cf;
+  cf.xPer=0;
+  cf.yPer=0;
+  cf.zPer=0;
+  rk_func *f = initTests(cf);
+
+  cf.bcR=BCType::outflow;
+  cf.bcL=BCType::outflow;
+  cf.bcB=BCType::outflow;
+  cf.bcT=BCType::outflow;
+  cf.bcH=BCType::outflow;
+  cf.bcF=BCType::outflow;
+
+  FS4DH varH = Kokkos::create_mirror_view(f->var);
+
+  // x-y
+  varH(3,3,3,0) = 1;
+  varH(4,3,3,0) = 2;
+  varH(5,3,3,0) = 3;
+  varH(3,4,3,0) = 4;
+  varH(4,4,3,0) = 5;
+  varH(5,4,3,0) = 6;
+  varH(3,5,3,0) = 7;
+  varH(4,5,3,0) = 8;
+  varH(5,5,3,0) = 9;
+
+  Kokkos::deep_copy(f->var,varH);
+  applyBCs(cf,f);
+  Kokkos::deep_copy(varH,f->var);
+
+  assert(varH(4,0,3,0)==8);
+  assert(varH(0,4,3,0)==6);
+  assert(varH(4,8,3,0)==2);
+  assert(varH(8,4,3,0)==4);
+  assert(varH(0,0,3,0)==9);
+  assert(varH(8,0,3,0)==7);
+  assert(varH(0,8,3,0)==3);
+  assert(varH(8,8,3,0)==1);
+
+  // y-z
+  varH(3,3,3,0) = 1;
+  varH(3,4,3,0) = 2;
+  varH(3,5,3,0) = 3;
+  varH(3,3,4,0) = 4;
+  varH(3,4,4,0) = 5;
+  varH(3,5,4,0) = 6;
+  varH(3,3,5,0) = 7;
+  varH(3,4,5,0) = 8;
+  varH(3,5,5,0) = 9;
+
+  Kokkos::deep_copy(f->var,varH);
+  applyBCs(cf,f);
+  Kokkos::deep_copy(varH,f->var);
+
+  assert(varH(3,4,0,0)==8);
+  assert(varH(3,0,4,0)==6);
+  assert(varH(3,4,8,0)==2);
+  assert(varH(3,8,4,0)==4);
+  assert(varH(3,0,0,0)==9);
+  assert(varH(3,8,0,0)==7);
+  assert(varH(3,0,8,0)==3);
+  assert(varH(3,8,8,0)==1);
+
+  return 0;
+}
--- tests/tests.cmake
+++ tests/tests.cmake
@@ -1,25 +1,41 @@
 include(CTest)
 enable_testing()
 
-add_subdirectory(catch2)
-add_executable(tests tests/test.cpp src/cart3d.cpp)
-if (NOT Fiesta_NO_MPI)
-    target_compile_definitions(tests     PRIVATE HAVE_MPI)
-endif()
-target_link_libraries(tests PRIVATE Catch2::Catch2WithMain Kokkos::kokkos FiestaCore)
-
-include(catch2/extras/Catch.cmake)
-catch_discover_tests(tests)
+include_directories("tests/common")
+set(TEST_NAMES 
+    bc_outflow
+    bc_reflective
+    bc_hydrostatic
+    )
 
 if (NOT Fiesta_NO_MPI)
+    foreach(test_name IN LISTS TEST_NAMES)
+        add_executable(${test_name} tests/${test_name}.cpp tests/common/test.cpp src/cart3d.cpp)
+        target_link_libraries(${test_name} PRIVATE Kokkos::kokkos FiestaCore)
+        add_test(NAME ${test_name} COMMAND ./tests/${test_name})
+        set_target_properties( ${test_name}
+            PROPERTIES
+            ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/tests"
+            LIBRARY_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/tests"
+            RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/tests"
+            )
+    endforeach()
+
     add_executable(halotest_unordered tests/halox_unordered.cpp src/cart3d.cpp)
     target_link_libraries(halotest_unordered PRIVATE Kokkos::kokkos FiestaCore)
-    add_test(NAME unordered_host_copy_halo_test COMMAND mpirun --oversubscribe -n 8 ./halotest_unordered 1)
-    add_test(NAME unordered_gpu_aware_halo_test COMMAND mpirun --oversubscribe -n 8 ./halotest_unordered 2)
-    add_test(NAME unordered_gpu_type_halo_test COMMAND mpirun --oversubscribe -n 8 ./halotest_unordered 3)
+    add_test(NAME halox_unordered_host_copy COMMAND mpirun --oversubscribe -n 8 ./tests/halotest_unordered 1)
+    add_test(NAME halox_unordered_gpu_aware COMMAND mpirun --oversubscribe -n 8 ./tests/halotest_unordered 2)
+    add_test(NAME halox_unordered_gpu_type COMMAND mpirun --oversubscribe -n 8 ./tests/halotest_unordered 3)
 
     add_executable(halotest_ordered tests/halox_ordered.cpp src/cart3d.cpp)
     target_link_libraries(halotest_ordered PRIVATE Kokkos::kokkos FiestaCore)
-    add_test(NAME ordered_host_copy_halo_test COMMAND mpirun --oversubscribe -n 8 ./halotest_ordered 1)
-    add_test(NAME ordered_gpu_aware_halo_test COMMAND mpirun --oversubscribe -n 8 ./halotest_ordered 2)
+    add_test(NAME halox_ordered_host_copy COMMAND mpirun --oversubscribe -n 8 ./tests/halotest_ordered 1)
+    add_test(NAME halox_ordered_gpu_aware COMMAND mpirun --oversubscribe -n 8 ./tests/halotest_ordered 2)
+
+    set_target_properties( halotest_ordered halotest_unordered
+        PROPERTIES
+        ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/tests"
+        LIBRARY_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/tests"
+        RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/tests"
+        )
 endif()
